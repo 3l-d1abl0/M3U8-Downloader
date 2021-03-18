3cb869594ca03d90e84ad3f99dfed1bd7c70d3d2
Sameer <sameer.barha12@gmail.com>
2021-03-18 13:23:40 +0530

    fetch master m3u8 file

--- hls_downloader.py
+++ hls_downloader.py
@@ -2,11 +2,42 @@
 # -*- coding: utf-8 -*-
 
 import os
+import ssl
 import sys
 import argparse
 import urllib.request
 
 class HlsDownloader(object):
+
+    @staticmethod
+    def getData(output_dir, master_file_name, master_url) -> str:
+
+        ssl._create_default_https_context = ssl._create_unverified_context
+        site = urllib.request.urlopen(master_url)
+        meta = site.info()
+
+        #size in Bytes
+        print( int(meta['Content-Length']) )
+
+        with site as response:
+            data = response.read()
+
+        with open(os.path.join(output_dir, master_file_name), 'wb') as file:
+            file.write(data)
+
+
+        return True
+
+    @staticmethod
+    def createFolder(path: str) -> bool:
+        if not os.path.exists(path):
+            try:
+                os.mkdir(path)
+            except Exception as e:
+                print("{1}\n Failed to create folder {2}. Exiting !!!".format(e, path))
+                return False
+
+        return True
 
     @staticmethod
     def isValidUrl(url: str) -> bool:
@@ -23,7 +54,7 @@ class HlsDownloader(object):
             return False
         else:
             return True
-    
+
     def __new__(cls, url):
 
         if HlsDownloader.isValidUrl(url):
@@ -37,6 +68,34 @@ class HlsDownloader(object):
         self.master_url = url
         print("Init HLS Class")
 
+    def process(self) -> bool:
+        baseUrl = os.path.dirname(self.master_url)
+        self.m3u8_master_file_name = os.path.basename(self.master_url)
+
+        print(baseUrl)
+        print(self.m3u8_master_file_name)
+
+        curPath = os.path.abspath(os.curdir)
+        binDir = os.path.join(curPath, self.master_url.split('/')[-2])
+
+        print(curPath)
+        print(binDir)
+
+        self.output_dir = os.path.join(os.path.abspath(os.curdir), self.master_url.split('/')[-2])
+
+        if HlsDownloader.createFolder(self.output_dir) is True:
+            print('reading File ... ')
+            #read master file
+            master_flag = HlsDownloader.getData(self.output_dir, self.m3u8_master_file_name, self.master_url)
+
+            if master_flag is False:
+                print("ERROR while handling m3u8 file !")
+                exit(-1)
+            else:   #process
+                
+        else:
+            exit(-1)
+
 
 if __name__ == '__main__':
 
@@ -45,10 +104,11 @@ if __name__ == '__main__':
     args = vars(ap.parse_args())
 
     print(args["url"])
-    
+
     m3u8_object = HlsDownloader(args["url"])
     if m3u8_object is None:
         print("Enter a valid m3u8 URL")
+        exit(-1)
     else:
         print(m3u8_object.master_url)
-    
+        m3u8_object.process()
