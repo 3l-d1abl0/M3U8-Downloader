5f3c448dfecc953e2e84f46ba360e39b3855fb8a
Sameer <sameer.barha12@gmail.com>
2021-03-19 16:14:17 +0530

    added downloading segments

--- hls_downloader.py
+++ hls_downloader.py
@@ -63,11 +63,45 @@ class HlsDownloader(object):
         with site as response:
             data = response.read()
 
+        print('=> getData ', os.path.join(self.output_dir, self.m3u8_master_file_name))
+
         with open(os.path.join(self.output_dir, self.m3u8_master_file_name), 'wb') as file:
             file.write(data)
 
 
         return True
+
+
+    def getSegments(self, option) -> None:
+
+        ssl._create_default_https_context = ssl._create_unverified_context
+        site = urllib.request.urlopen(self.source_list[option][1])
+        meta = site.info()
+
+        if not '?' in self.master_url :
+            media_name = os.path.basename(self.source_list[option][1])
+        else:
+            media_name = os.path.basename(self.source_list[option][1]).split('?')[0]
+
+        with site as response:
+            data = response.read()
+
+        with open(os.path.join(self.output_dir, os.path.basename(media_name)), 'wb') as file:
+            file.write(data)
+
+        with open(os.path.join(self.output_dir, os.path.basename(self.source_list[option][0]))+'.tmp', 'wb') as file:
+            for line in data.splitlines():
+                line = line.strip()
+
+                extension = os.path.splitext(line)[1]
+
+                if any(ext in extension.lower().decode("utf-8") for ext in ['.aac','.mp4','.ts']):
+                    media_url = os.path.join(self.base_url, line.decode())
+                    print('--> ', media_url)
+                    with urllib.request.urlopen(media_url) as response:
+                                file.write(response.read())
+
+        os.rename(os.path.join(self.output_dir, os.path.basename(self.source_list[option][0]))+'.tmp', os.path.join(self.output_dir, os.path.basename(self.source_list[option][0]))+str(extension))
 
     @staticmethod
     def checkOption(option, options_list) -> bool:
@@ -94,11 +128,12 @@ class HlsDownloader(object):
 
     @staticmethod
     def isValidUrl(url: str) -> bool:
+
         if url == '':
             print("URL ERROR : Empty String !")
             return False
 
-        elif os.path.splitext(url)[1].lower() != '.m3u8':
+        elif not '.m3u8' in url:
             print("URL ERROR : Not a hls/m3u8 master File !")
             return False
 
@@ -127,16 +162,19 @@ class HlsDownloader(object):
 
     def process(self) -> bool:
         self.base_url = os.path.dirname(self.master_url)
-        self.m3u8_master_file_name = os.path.basename(self.master_url)
+        if not '?' in self.master_url :
+            self.m3u8_master_file_name = os.path.basename(self.master_url)
+        else:
+            self.m3u8_master_file_name = os.path.basename(self.master_url).split('?')[0]
 
-        #print(self.base_url)
-        #print(self.m3u8_master_file_name)
+        print('Base : ', self.base_url)
+        print('Master : ',self.m3u8_master_file_name)
 
         curPath = os.path.abspath(os.curdir)
         binDir = os.path.join(curPath, self.master_url.split('/')[-2])
 
-        #print(curPath)
-        #print(binDir)
+        print(curPath)
+        print(binDir)
 
         self.output_dir = os.path.join(os.path.abspath(os.curdir), self.master_url.split('/')[-2])
 
@@ -156,6 +194,13 @@ class HlsDownloader(object):
                     exit(-1)
                 else:
                     print("Processing your option(s) : {}\n".format(download_options))
+
+                    for val in download_options:
+                        print(self.source_list[val][1])
+
+                        self.getSegments(val)
+
+                    print('-----------------')
         else:
             exit(-1)
 
@@ -173,5 +218,5 @@ if __name__ == '__main__':
         print("Enter a valid m3u8 URL")
         exit(-1)
     else:
-        print(m3u8_object.master_url)
+        print('Processing ... ', m3u8_object.master_url)
         m3u8_object.process()
